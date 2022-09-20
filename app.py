cfd54f676c85c51ab02aed626b0c89035455f3c1
Oliver_S <90482439+carlshoow@users.noreply.github.com>
2022-09-20 11:04:25 +0200

    Add files via upload
    Signed-off-by: Oliver_S <90482439+carlshoow@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,52 +1,65 @@
-from ensurepip import bootstrap
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-
+#__init__
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_sqlite'
 db = SQLAlchemy(app)
 
 
-
-class Todo(db.Model):
+#Model database
+class TodoDatabase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(130))
-    complete = db.Column(db.Boolean)
+    task = db.Column(db.String(100))
+    status = db.Column(db.Boolean)
 
 
-@app.route("/")
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form.get('title')
+    new_task = TodoDatabase(task=task, status=False)
+    db.session.add(new_task)
+    try:
+        db.session.commit()
+    except:
+        print(new_task.task, new_task.status)
+    
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    to_delete = TodoDatabase.query.filter_by(id=task_id).first()
+    db.session.delete(to_delete)
+    try:
+        db.session.commit()
+    except:
+        print("delete was a fail")
+    
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:task_id>')
+def update_task(task_id):
+    to_update = TodoDatabase.query.filter_by(id=task_id).first()
+    to_update.status = not to_update.status
+    
+    try:
+        db.session.commit()
+    except:
+        print("delete was a fail")
+    
+    return redirect(url_for('index'))
+
+
+
+#main page
+@app.route('/')
 def index():
-    todo_list = Todo.query.all()
-    return render_template('index.html', todo_list=todo_list)
+    todo_tasks = TodoDatabase.query.all()
+    return render_template('index.html', tasks=todo_tasks)
 
 
-@app.route("/add", methods=["POST"])
-def add():
-    title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for("index"))
-
-@app.route("/update/<int:todo_id>")
-def update(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    todo.complete = not todo.complete
-    db.session.commit()
-    return redirect(url_for("index"))
-
-@app.route("/delete/<int:todo_id>")
-def delete(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect(url_for("index"))
-
-
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     db.create_all()
-    app.run(debug=1)
+    app.run(debug=True)
 
